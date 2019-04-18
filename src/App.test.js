504521586660af61d66b17df587d4b7a1bd384de
Olabode Adeloye <bodeadeloye@gmail.com>
2019-04-18 12:01:09 +0100

    Adding Test Scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import TrainingForm from "./components/pages/TrainingForms/TrainingForm";
+import ContactForm from "./components/ContactForm";
+import NavBar from "./components/Layout/NavBar/NavBar";
+import SignInWidget from "./components/auth/SignInWidget";
+//import Login from "./components/auth/Login";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("TrainingForm", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<TrainingForm debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("ContactForm", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<ContactForm debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("NavBar", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<NavBar debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+// describe("Login", () => {
+//   it('should render correctly in "debug" mode', () => {
+//     const component = shallow(<Login debug />);
+
+//     expect(component).toMatchSnapshot();
+//   });
+// });
+
+describe("SignInWidget", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<SignInWidget debug />);
+
+    expect(component).toMatchSnapshot();
+  });
 });
