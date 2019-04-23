904e16bea10dd17463f5b19d3ae0bc014b9e0250
Olabode Adeloye <bodeadeloye@gmail.com>
2019-04-23 12:55:47 +0100

    Netlify test updates

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import SignInWidget from "./SignInWidget";
+import SignInWidget from "SignInWidget"
 import { withAuth } from "@okta/okta-react";
 
 export default withAuth(
