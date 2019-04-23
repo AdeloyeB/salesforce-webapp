3f811f010604d3dc09b027ab3971cad2d47c8f18
Olabode Adeloye <bodeadeloye@gmail.com>
2019-04-23 13:19:34 +0100

    more

--- src/components/pages/ReportAProblem/ReportAProblem.js
+++ src/components/pages/ReportAProblem/ReportAProblem.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 import NavBar from "../../Layout/NavBar/NavBar";
 import Footer from "../../Layout/Footer/Footer";
 
-import ContactForm from "../ContactForm";
-import ContactJumbo from "../ContactJumbo";
+import ContactForm from "../../ContactForm";
+import ContactJumbo from "../../ContactJumbo";
 
 export class ReportAProblem extends Component {
   render() {
